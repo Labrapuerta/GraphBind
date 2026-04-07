a32f895daedc13fd2e15edb6ca9cb9dd6ed00cbc
Labrapuerta <jmanuelhlabra@hotmail.com>
2026-04-08 00:19:05 +0200

    Cleaned repository, removed unnecessary files and updated .gitignore to prevent future clutter.

---
+++ model/config.py
@@ -0,0 +1,14 @@
+CONFIG = {
+    # ── Model ─────────────────────────────────────────────────────────
+    "node_input_dim":        1305,
+    "edge_input_dim":        4,
+    "hidden_dim":            512,
+    "num_egnn_layers":       3,
+    "num_evoformer_blocks":  8,
+    "num_heads":             16,
+    "dropout":               0.3,
+    "update_coords":         True,
+    "num_recycles":          4,
+    "alpha":                 0.3,
+ 
+}
